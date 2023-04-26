9cba5a0d200808caedcee910783ca7b15a4f98c2
nerfshacopls <dsjgdavid@gmail.com>
2023-04-26 18:12:27 +0200

    custom.yaml y test yolo

--- test_yolo.py
+++ test_yolo.py
@@ -2,12 +2,39 @@
 from ultralytics import YOLO
 
 import cv2
+import os
 
+
+ruta = "..\collab\content\\runs\detect\\train2\weights\\best.pt"
+imagen = "..\imagenes\img2\Aumentadas_check\\"
+model = YOLO(ruta)
+
+
+files_list = os.listdir(imagen)
+
+
+for file in files_list:
+    if file.endswith(".png"):
+        results = model(imagen+file)
+
+        res_plotted = results[0].plot()
+
+        res_plotted = cv2.resize(res_plotted, (960, 540))
+        cv2.imshow("result", res_plotted)
+        cv2.waitKey(0)
+
+'''
 model = YOLO('yolov8n.pt')
 
-results = model.train(data='coco128.yaml', epochs=3)  # train the model
+
+results = model.train(data='custom.yaml', epochs=3, device=0)  # train the model
 results = model.val()  # evaluate model performance on the validation set
-'''
+
+for r in results:
+    ploted = r.plot()
+    cv2.imshow("result", ploted)
+    cv2.waitKey(0)
+
 model = YOLO('yolov8n.pt') # pass any model type
 #results = model.train(data='coco128.yaml', epochs=3)
 #results = model.val()  # evaluate model performance on the validation set
